80a4c8938ef46085be354229e4cdf0dafde40a04
Nacho <ignacio.nahuel.ramos@gmail.com>
2024-04-21 16:34:28 -0300

    use mercadopago sdk

--- api/filter_payments.py
+++ api/filter_payments.py
@@ -1,3 +1,4 @@
+from datetime import datetime, timedelta
 
 
 def filter_all_payments(products, payments):
@@ -16,31 +17,38 @@ def filter_all_payments(products, payments):
         # Procesar cada pago
         for payment in payments:
             if 'shopify_data' in payment['metadata'] and payment['status'] == 'approved':
-                gross_amount = payment['transaction_details']['total_paid_amount']
-                net_amount = payment['transaction_details']['net_received_amount']
                 
-                time.append(payment['date_created'])
-                gross_amounts.append(gross_amount)
-                net_amounts.append(net_amount)
+                # Filtrar los pagos de ayer
+                yesterday = datetime.now() - timedelta(days=1)
+                date_created = datetime.strptime(payment['date_created'], "%Y-%m-%dT%H:%M:%S.%f%z")
+                date_created = date_created.replace(tzinfo=None) + timedelta(hours=1) # Convertir a UTC-3
 
-                # Print email && date_created
-                # print(payment['payer']['email'], payment['date_created'], gross_amount, net_amount)
+                if date_created.date() == yesterday.date():
+                    gross_amount = payment['transaction_details']['total_paid_amount']
+                    net_amount = payment['transaction_details']['net_received_amount']
+                    
+                    time.append(payment['date_created'])
+                    gross_amounts.append(gross_amount)
+                    net_amounts.append(net_amount)
 
-                # Buscar el producto por el valor neto
-                product_name = None
-                for product in products:
-                    for price in product['prices']:
-                        # Verificar si el monto bruto es un múltiplo del precio
-                        if gross_amount % price == 0:
-                            product_name = product['name']
+                    # Print email && date_created
+                    # print(payment['payer']['email'], payment['date_created'], gross_amount, net_amount)
+
+                    # Buscar el producto por el valor neto
+                    product_name = None
+                    for product in products:
+                        for price in product['prices']:
+                            # Verificar si el monto bruto es un múltiplo del precio
+                            if gross_amount % price == 0:
+                                product_name = product['name']
+                                break
+                        if product_name:
                             break
+                    
                     if product_name:
-                        break
-                
-                if product_name:
-                    totals_by_product[product_name] += net_amount
-                else:
-                    print(f"Warning: No product found for gross amount {gross_amount}!")
+                        totals_by_product[product_name] += net_amount
+                    else:
+                        print(f"Warning: No product found for gross amount {gross_amount}!")
         
         # Redondear a dos decimales y agregar comas como separadores de miles
         for product_name, total in totals_by_product.items():
--- api/get_payments.py
+++ api/get_payments.py
@@ -1,51 +1,32 @@
-import requests
-from datetime import datetime, timedelta
-import pytz
+import mercadopago
 
 
 def get_all_payments(access_token):
     try:
         print('Start get_all_payments')
-        headers = {
-            'accept': 'application/json',
-            'Authorization': f'Bearer {access_token}'
-        }
+        sdk = mercadopago.SDK(access_token)
 
-        # Obtener la fecha y hora actual en la zona horaria 'America/New_York' (UTC-4)
-        tz_new_york = pytz.timezone('America/New_York')
-        current_date_new_york = datetime.now(tz_new_york)
-
-        # Ajustar las horas, minutos y segundos para obtener el inicio y fin del día anterior en 'America/New_York' (UTC-4)
-        end_date_new_york = current_date_new_york.replace(hour=0, minute=0, second=0, microsecond=0)
-        begin_date_new_york = end_date_new_york - timedelta(days=1)
-
-        # Convertir las fechas a la zona horaria de Argentina (UTC-3)
-        tz_argentina = pytz.timezone('America/Argentina/Buenos_Aires')
-        begin_date_argentina = begin_date_new_york.astimezone(tz_argentina) + timedelta(hours=2)
-        end_date_argentina = end_date_new_york.astimezone(tz_argentina) + timedelta(hours=2)
-
-        # Utilizar las fechas para la búsqueda en la API
-        params = {
-            'begin_date': begin_date_argentina.strftime('%Y-%m-%dT%H:%M:%SZ'),
-            'end_date': end_date_argentina.strftime('%Y-%m-%dT%H:%M:%SZ'),
-            'limit': 100,
-            'offset': 0
+        filters = {
+            "sort": "date_created",
+            "criteria": "desc",
+            "range": "date_created",
+            "begin_date": "NOW-2DAYS",
+            "end_date": "NOW",
+            "limit": 100,
+            "offset": 0
         }
 
         all_payments = []
 
         while True:
-            response = requests.get('https://api.mercadopago.com/v1/payments/search', headers=headers, params=params)
-            data = response.json()
-            
-            all_payments.extend(data['results'])
-
-            # Si el número de resultados es menor que el límite, significa que ya no hay más pagos para obtener
-            if len(data['results']) < params['limit']:
-                break
-
-            # Aumentar el offset para la próxima página
-            params['offset'] += params['limit']
+           search_request = sdk.payment().search(filters)
+           all_payments.extend(search_request['response']['results'])
+           
+           if len(search_request['response']['results']) < filters['limit']:
+            break
+           
+           filters['offset'] += filters['limit']
+        
         return all_payments
     
     except Exception as e:
