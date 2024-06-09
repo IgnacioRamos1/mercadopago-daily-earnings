f818cbb32ef51d59be9a4dc5490194b8bc4475e8
Ignacio Ramos <ignacio.nahuel.ramos@gmail.com>
2024-06-09 11:49:50 -0300

    removed send email function

--- process_payments/process_payments.py
+++ process_payments/process_payments.py
@@ -1,6 +1,5 @@
 from api.get_payments import get_all_payments
 from api.filter_payments import filter_all_payments
-from utils.send_email import send_email
 from google_drive.update_google_sheet import update_google_sheet
 from mongo_db.utils.security import decrypt_string
 from mongo_db.get_store_products import get_store_products
@@ -24,12 +23,6 @@ def process_payments(store):
             print(f"Updating spreadsheet for {product_name}")
             update_google_sheet(product_name, total, store["name"])
             print(f"Finished updating spreadsheet for {product_name}")
-
-        # Enviar un solo mensaje de correo electrónico con todos los totales
-        message = f"Totales para {store['name']}:\n"
-        for product_name, total in totals_by_product.items():
-            message += f"{product_name}: ${total}\n"
-        send_email(message, store["email"], store["name"])
         
         print('Finished process_payments function')
         return totals_by_product
