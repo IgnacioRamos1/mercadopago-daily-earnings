eb92dea6d8bc62136607854d3799e650f2bb6f2f
Ignacio Ramos <ignacio.nahuel.ramos@gmail.com>
2024-06-14 10:20:42 -0300

    test

--- api/filter_payments.py
+++ api/filter_payments.py
@@ -22,8 +22,8 @@ def filter_all_payments(products, payments, provider):
             else:
                 if 'shopify_data' in payment['metadata']:
                     continue
-                if provider.replace(' ', '') in payment['metadata']['plugin_data']['original_notification_url']:
-                    print(payment['metadata']['plugin_data']['original_notification_url'])
+                if provider.replace(' ', '') in payment['metadata']['original_notification_url']:
+                    print(payment['metadata']['original_notification_url'])
                     counter = filter_payments(payment, gross_amounts, net_amounts, time, products, totals_by_product)
                     if counter == 1:
                         print(payment)
